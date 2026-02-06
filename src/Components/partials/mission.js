bd2b210e9f920ca6014d4724271d5e2c6fc2be39
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2026-02-06 18:25:47 -0500

    mission

---
+++ src/Components/partials/mission.js
@@ -0,0 +1,29 @@
+//MISSION SECTION RELATED
+import React from "react";
+
+//MISSION
+export default function Mission() {
+  //----vars:
+
+  return (
+    <div className="Mission-Section">
+      <div className="WRAPPER">
+        <div className="img"></div>
+        <div className="content">
+          <div className="Header">
+            <h1 className="header-title">
+              Your Vision, <span className="Colored">Our Mission</span>
+            </h1>
+            <p className="header-sub">Dedicated to Bringing Ideas to Life</p>
+          </div>
+          <p className="desc">
+            We believe every project is more than just paint and
+            construction—it’s about creating spaces that inspire. From first
+            brushstroke to final detail, our mission is to deliver results that
+            reflect your style, meet your needs, and exceed your expectations.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}

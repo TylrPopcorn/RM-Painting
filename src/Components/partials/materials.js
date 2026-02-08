04ca283b1964bbbfcf931f161b648cd62c0a99d5
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2026-02-07 20:16:10 -0500

    materials

---
+++ src/Components/partials/materials.js
@@ -0,0 +1,31 @@
+//MATERIALS SECTION RELATED
+import React from "react";
+
+//MATERIALS
+export default function Materials() {
+  //----vars:
+
+  return (
+    <div className="Materials-Section">
+      <div className="WRAPPER">
+        <div className="content">
+          <div className="Header">
+            <h1 className="header-title">
+              <span className="Colored">Quality</span> Materials
+            </h1>
+            <p className="header-sub">
+              Bringing Color and Quality to Every Space
+            </p>
+          </div>
+          <p className="desc">
+            We use only premium materials and proven techniques to create
+            finishes that stand the test of time. From vibrant interiors to
+            durable exteriors, our team blends skill, precision, and attention
+            to detail in every project.
+          </p>
+        </div>
+        <div className="img"></div>
+      </div>
+    </div>
+  );
+}

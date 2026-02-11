34ca1ea29e7ad6adc1928d76d0afa49d4e41a36a
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2026-02-11 17:23:11 -0500

    craftmanship

---
+++ src/Components/partials/craftmanship.js
@@ -0,0 +1,29 @@
+//CRAFTMANSHIP SECTION RELATED
+import React from "react";
+
+//CRAFTMANSHIP
+export default function Craftmanship() {
+  //----vars:
+
+  return (
+    <div className="Craftmanship-Section">
+      <div className="WRAPPER">
+        <div className="content">
+          <div className="Header">
+            <h1 className="header-title">Lasting Craftsmanship</h1>
+            <p className="header-sub">
+              Projects Built to Stand the Test of Time
+            </p>
+          </div>
+          <p className="desc">
+            Every project we take on is built with quality and durability in
+            mind. Whether it’s a fresh coat of paint or a major remodel, our
+            team combines proven techniques, top-grade materials, and skilled
+            craftsmanship to deliver results that last.
+          </p>
+        </div>
+        <div className="img"></div>
+      </div>
+    </div>
+  );
+}

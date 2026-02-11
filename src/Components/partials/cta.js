02ece581400b677fa5bdf08016281c4d3a59f8b6
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2026-02-10 23:21:39 -0500

    cta

---
+++ src/Components/partials/cta.js
@@ -0,0 +1,27 @@
+//CTA SECTION RELATED
+import React from "react";
+
+//CTA
+export default function CTA() {
+  //----vars:
+
+  return (
+    <div className="CTA-Section">
+      <div className="WRAPPER">
+        <div className="img">
+          <h3 className="header-title">
+            Get Your{" "}
+            <span className="Colored">
+              Free <br></br>Estimate
+            </span>{" "}
+            Today.
+          </h3>
+          <p className="desc">
+            Let's bring your vision to life with expert painting and remodeling
+            services.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}

56b1169827e5f0298e84de160452d28beec99f26
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2026-02-13 07:29:12 -0500

    about

---
+++ src/Components/partials/aboutUs.js
@@ -0,0 +1,28 @@
+//ABOUT US SECTION RELATED
+import React from "react";
+
+//ABOUT US
+export default function AboutUs() {
+  //----vars:
+
+  return (
+    <div className="AboutUs-Section">
+      <div className="WRAPPER">
+        <div className="img"></div>
+        <div className="content">
+          <div className="Header">
+            <h1 className="header-title">About Us</h1>
+            <p className="header-sub">Built on Trust and Integrity</p>
+          </div>
+          <p className="desc">
+            We deliver painting, construction and peace of mind. With decades of
+            experience, we combine craftsmanship, integrity, and reliable
+            service to bring your vision to life. As a family-owned business, we
+            treat every client like a neighbor, ensuring clear communication,
+            lasting results, and projects done with care from start to finish.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
